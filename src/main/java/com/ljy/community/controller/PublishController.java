ee6d0d8ed0bc16dd2bc696f9e0498b6fb072b632
ljy <1578828228@qq.com>
2020-04-02 18:48:15 +0800

    完成发布文章功能并保存到数据库

--- src/main/java/com/ljy/community/controller/PublishController.java
+++ src/main/java/com/ljy/community/controller/PublishController.java
@@ -1,13 +1,80 @@
 package com.ljy.community.controller;
 
+import com.ljy.community.mapper.QuestionMapper;
+import com.ljy.community.mapper.UserMapper;
+import com.ljy.community.model.Question;
+import com.ljy.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
 
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/publish")
     public String publish(){
         return "publish";
+    }
+    @PostMapping("/publish")
+    public String doPublish(
+            @RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam("tag") String tag,
+            HttpServletRequest request,
+            Model model){
+        model.addAttribute("title",title);
+        model.addAttribute("description",description);
+        model.addAttribute("tag",tag);
+        if (title==null||title==""){
+            model.addAttribute("error","标题不能为空");
+            return "publish";
+        }
+        if (description==null||description==""){
+            model.addAttribute("error","内容补充不能为空");
+            return "publish";
+        }
+        if (tag==null||tag==""){
+            model.addAttribute("error","标签不能为空");
+            return "publish";
+        }
+
+        User user = null;
+        Cookie[] cookies=request.getCookies();
+        for (Cookie cookie:cookies) {
+            if (cookie.getName().equals("token")){
+                String token=cookie.getValue();
+                //根据token去数据库中查找是否有该用户，判断是否存在该用户,从而实现持久化登录
+                user=userMapper.findByToken(token);
+                if(user!=null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+        if (user==null){
+            model.addAttribute("error","用户未登录");
+            return "publish";
+        }
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(question.getGmtCreate());
+        questionMapper.creat(question);
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/ljy/community/mapper/QuestionMapper.java
@@ -0,0 +1,10 @@
+package com.ljy.community.mapper;
+import com.ljy.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuestionMapper {
+    @Insert("insert into question(title,description,gmt_create,gmt_modified,creator,tag) values(#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
+     void creat(Question question);
+}
