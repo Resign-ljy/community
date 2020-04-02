b13a0926c8ee66b0c55c2fb853ac38efab59752e
ljy <1578828228@qq.com>
2020-04-02 14:27:54 +0800

    集成Mybaits实现插入操作

--- src/main/java/com/ljy/community/controller/AuthorizeContraller.java
+++ src/main/java/com/ljy/community/controller/AuthorizeContraller.java
@@ -2,6 +2,8 @@ package com.ljy.community.controller;
 
 import com.ljy.community.dto.AccessTokenDTO;
 import com.ljy.community.dto.GithubUser;
+import com.ljy.community.mapper.UserMapper;
+import com.ljy.community.model.User;
 import com.ljy.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeContraller {
@@ -26,12 +29,13 @@ public class AuthorizeContraller {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public  String callback(@RequestParam(name="code") String code,
                             @RequestParam(name="state") String state,
                             HttpServletRequest request){
-        System.out.println(code);
-        System.out.println(state);
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -39,11 +43,22 @@ public class AuthorizeContraller {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if(user!=null){
-            request.getSession().setAttribute("user",user);
-            return "redirect:/";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        System.out.println(githubUser);
+        if(githubUser!=null){
+            User user = new User();
+            System.out.println(user);
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+
+            userMapper.insert(user);
             //登录成功，写cookie和session
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+
 
         }else{
             //登录失败，重新登录
