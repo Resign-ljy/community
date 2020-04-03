f6a19490d26ea35e4ed0890029498795dcb1358b
ljy <1578828228@qq.com>
2020-04-03 17:14:47 +0800

    添加首页列表添加功能

--- src/main/java/com/ljy/community/controller/IndexController.java
+++ src/main/java/com/ljy/community/controller/IndexController.java
@@ -1,20 +1,31 @@
 package com.ljy.community.controller;
 
+import com.ljy.community.dto.QuestionDTO;
+import com.ljy.community.mapper.QuestionMapper;
 import com.ljy.community.mapper.UserMapper;
+import com.ljy.community.model.Question;
 import com.ljy.community.model.User;
+import com.ljy.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private QuestionService questionService;
+
     @GetMapping("/")
-    public String index(HttpServletRequest request){
+    public String index(HttpServletRequest request,
+                        Model model){
         //得到token
         Cookie[] cookies=request.getCookies();
         if (cookies != null && cookies.length != 0)
@@ -29,8 +40,8 @@ public class IndexController {
                     break;
                 }
             }
-
-
+        List<QuestionDTO> questionList=questionService.list();
+            model.addAttribute("question",questionList);
         return "index";
     }
 
