9d6c0ad4a6224348523050559c3bd165c87b074a
ljy <1578828228@qq.com>
2020-04-11 18:30:22 +0800

    规范提问的标签库

--- src/main/java/com/ljy/community/controller/PublishController.java
+++ src/main/java/com/ljy/community/controller/PublishController.java
@@ -1,10 +1,12 @@
 package com.ljy.community.controller;
 
+import com.ljy.community.cache.TagCache;
 import com.ljy.community.dto.QuestionDTO;
 import com.ljy.community.mapper.QuestionMapper;
 import com.ljy.community.model.Question;
 import com.ljy.community.model.User;
 import com.ljy.community.service.QuestionService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -22,51 +25,58 @@ public class PublishController {
 
 
     @GetMapping("/publish/{id}")
-    public String edit(@PathVariable(name = "id")Long id,
-                       Model model){
-        QuestionDTO question=questionService.getById(id);
-        model.addAttribute("title",question.getTitle());
-        model.addAttribute("description",question.getDescription());
-        model.addAttribute("tag",question.getTag());
-        model.addAttribute("id",question.getId());
+    public String edit(@PathVariable(name = "id") Long id,
+                       Model model) {
+        QuestionDTO question = questionService.getById(id);
+        model.addAttribute("title", question.getTitle());
+        model.addAttribute("description", question.getDescription());
+        model.addAttribute("tag", question.getTag());
+        model.addAttribute("id", question.getId());
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
 
 
-
     @GetMapping("/publish")
-    public String publish(){
+    public String publish(Model model) {
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
 
     @PostMapping("/publish")
     public String doPublish(
-            @RequestParam(value = "title",required = false) String title,
-            @RequestParam(value ="description",required = false) String description,
-            @RequestParam(value ="tag",required = false) String tag,
-            @RequestParam(value = "id",required = false)Long id,
+            @RequestParam(value = "title", required = false) String title,
+            @RequestParam(value = "description", required = false) String description,
+            @RequestParam(value = "tag", required = false) String tag,
+            @RequestParam(value = "id", required = false) Long id,
             HttpServletRequest request,
-            Model model){
-        model.addAttribute("title",title);
-        model.addAttribute("description",description);
-        model.addAttribute("tag",tag);
-        if (title==null||title==""){
-            model.addAttribute("error","标题不能为空");
+            Model model) {
+        model.addAttribute("title", title);
+        model.addAttribute("description", description);
+        model.addAttribute("tag", tag);
+        model.addAttribute("tags", TagCache.get());
+        if (title == null || title == "") {
+            model.addAttribute("error", "标题不能为空");
             return "publish";
         }
-        if (description==null||description==""){
-            model.addAttribute("error","内容补充不能为空");
+        if (description == null || description == "") {
+            model.addAttribute("error", "内容补充不能为空");
             return "publish";
         }
-        if (tag==null||tag==""){
-            model.addAttribute("error","标签不能为空");
+        if (tag == null || tag == "") {
+            model.addAttribute("error", "标签不能为空");
             return "publish";
         }
 
+        String invalid=TagCache.filterInvalid(tag);
+        if (StringUtils.isNoneBlank(invalid)){
+            model.addAttribute("error", "输入非法标签"+invalid);
+            return "publish";
+        }
 
         User user = (User) request.getSession().getAttribute("user");
-        if (user==null){
-            model.addAttribute("error","用户未登录");
+        if (user == null) {
+            model.addAttribute("error", "用户未登录");
             return "publish";
         }
         Question question = new Question();
